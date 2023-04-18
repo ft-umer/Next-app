382505fdfc3ac0d06ed65252e552cad0112cd071
Syed Umer Mujahide <syedsb1122@gmail.com>
2023-04-18 05:16:44 +0500

    Next App

---
+++ pages/Blog.tsx
@@ -0,0 +1,15 @@
+import Head from "next/head"
+import Link from "next/link"
+
+const Blogs = () => {
+    return (
+        <><Head>
+            <title>Control Resell - Blog</title>
+        </Head><><h1>Blogs</h1>
+                <Link href="/">
+                    Home
+                </Link></></>
+    )
+}
+
+export default Blogs
